3e64d4b42dcc6329df2013cc10477d00c6514141
Malwande Mabanti <malwandem@payspace.com>
2023-06-23 10:05:26 +0200

    Add project files.

---
+++ Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(20, ErrorMessage = "Title can't be longer 20 characters")]
+        public string Title { get; set; }
+
+        [StringLength(50, ErrorMessage = "Title can't be longer 50 characters")]
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+
+namespace ToDoList
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            services.AddCors(options => 
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://FrontEndDomain.com")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                    });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if(env.IsDevelopment()) 
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+            }
+
+            
+            app.UseHttpsRedirection();
+
+            // Add these before the app.Endpoints() line:
+            app.UseCors("AllowSpecificOrigin");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            
+
+
+           
+
+            
+        }
+    }
+}
